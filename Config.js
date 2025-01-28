f413543970515ef354ae9e8678eaf1de559868c3
yvngskidler97 <frank.emmanuel5230@gmail.com>
2025-01-28 06:37:13 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "2349067339193", // your number with country code
-  OWNER_NAME: 'Miracle', // your owner name 
-  BOT_NAME: "KORD", // your preferred bot name
-  SESSION_ID: 'your_sessionid', // replace your_sessionid with yours and make sure its inside rhe quotes
+  OWNER_NUMBERS: "2347082081297", // your number with country code
+  OWNER_NAME: 'Frank', // your owner name 
+  BOT_NAME: "TELLIN", // your preferred bot name
+  SESSION_ID: 'kord_ai-ebJhpKrCWRiz', // replace your_sessionid with yours and make sure its inside rhe quotes
   INVINCIBLE_MODE: false, // toogle invincible mode 
   WORK_MODE: "Public",
   TIME_ZONE: "Africa/Lagos",
@@ -21,9 +21,9 @@ global.settings = {
   AUTO_LIKE_STATUS: true,
   AUTO_LIKE_EMOJI: '🍁',
   BANKING_DETAILS: {
-    bank: "Kord-Ai Bank", // the name of your preferred bank
-    acc_number: "1234567890", // account number for your preferred bank
-    acc_name: "MyAccountName" // name of your bank
+    bank: "Opay", // the name of your preferred bank
+    acc_number: "8108668961", // account number for your preferred bank
+    acc_name: "Evelyn Frank" // name of your bank
   },
   /* You can also replace the numbers with your addresses if you're using crypto, a command for this will come soon. */
   API_KEYS: {
@@ -32,7 +32,7 @@ global.settings = {
   isHackEnable: true,
   SAFE_SEARCH: true,
   REJECT_CALLS: true,
-  SEND_WELCOME_MESSAGE: false,
+  SEND_WELCOME_MESSAGE: true,
   WELCOME_MESSAGE: "👋 Welcome to the group! We're glad to have you here. 😊",
   DELETE_WA_LINKS: false,
   BAD_WORD_FILTER: true,
